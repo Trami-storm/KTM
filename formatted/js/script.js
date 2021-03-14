ca062fda60892bd83b8bc24eb158dc6c73dc0991
Mariia Teteriuk <62767556+Trami-storm@users.noreply.github.com>
2021-03-14 17:53:08 +0200

    Added script for dynaic load as a separate file

---
+++ formatted/js/script.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function () {
+                $("#rubrics").load("https://ktm.ukma.edu.ua/index.php/ktm/$$$call$$$/api/file/file-api/download-file?fileId=63&revision=1&submissionId=9&stageId=5");;
+                $("#header").load("https://ktm.ukma.edu.ua/index.php/ktm/$$$call$$$/api/file/file-api/download-file?fileId=62&revision=1&submissionId=9&stageId=5");;
+            });*/
+
+$(document).ready(function () {
+    $("#rubrics").load("sidebar.txt");;
+    $("#header").load("header.txt");;
+});
